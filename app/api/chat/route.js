524f9ba5ad4379eeba4b4ea3870a58319db6aa0b
GHGFaber <moises.fuentes904@gmail.com>
2024-08-22 18:55:48 -0700

    base code functionality: rag based on reviews and openai chatbot

---
+++ app/api/chat/route.js
@@ -0,0 +1,78 @@
+import { NextResponse } from "next/server";
+import { Pinecone } from "@pinecone-database/pinecone";
+import OpenAI from "openai";
+
+const systemPrompt = `You are a helpful assistant designed to assist students in finding the best professors based on their queries. When a student asks about a professor, you should identify and recommend the top professors that match the student's needs, focusing on factors such as teaching quality, helpfulness, and course difficulty. Your responses should be clear and concise, offering relevant details such as the professor's name, course subjects, rating highlights, and why they stand out based on the query.
+
+If the query is about specific subjects, courses, or qualities (e.g., "best math professors"), prioritize professors who excel in those areas.
+If the query is general (e.g., "top professors"), recommend professors with high overall ratings and broad student satisfaction.
+In all responses, balance being informative with being friendly and approachable.
+Your goal is to ensure students receive accurate and useful recommendations tailored to their academic preferences.`;
+
+export async function POST(req) {
+  const data = await req.json();
+  const pc = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY,
+  });
+  const index = pc.index("rag").namespace("ns1");
+  const openai = new OpenAI();
+
+  const text = data[data.length - 1].content;
+
+  const embedding = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input: text,
+    encoding_format: "float",
+  });
+
+  const results = await index.query({
+    topK: 3,
+    includeMetadata: true,
+    vector: embedding.data[0].embedding,
+  });
+
+  let resultString = "";
+  results.matches.forEach((match) => {
+    resultString += `
+    
+    Professor:${match.id}
+    Review:${match.metadata.review}
+    Subject:${match.metadata.subject}
+    Stars${match.metadata.stars}
+    `;
+  });
+
+  const lastMessage = data[data.length - 1];
+  const lastMessageContent = lastMessage.content + resultString;
+  const lastDataWithoutLastMessage = data.slice(0, data.length - 1);
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: systemPrompt },
+      ...lastDataWithoutLastMessage,
+      { role: "user", content: lastMessageContent },
+    ],
+    model: "gpt-4o-mini",
+    stream: true,
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+      try {
+        for await (const chunk of completion) {
+          const content = chunk.choices[0]?.delta?.content;
+          if (content) {
+            const text = encoder.encode(content);
+            controller.enqueue(text);
+          }
+        }
+      } catch (err) {
+        controller.error(err);
+      } finally {
+        controller.close();
+      }
+    },
+  });
+
+  return new NextResponse(stream);
+}
---
+++ app/page.js
@@ -0,0 +1,120 @@
+"use client";
+import RenderResult from "next/dist/server/render-result";
+import { useState } from "react";
+import { Box, TextField, Button, Stack } from "@mui/material";
+import ReactMarkdown from "react-markdown";
+
+export default function Home() {
+  const [messages, setMessages] = useState([
+    {
+      role: "assistant",
+      content:
+        "Hi! I'm the Rate My Professor support asssistant. How can I help you today?",
+    },
+  ]);
+  const [message, setMessage] = useState("");
+  const sendMessage = async () => {
+    setMessages((messages) => [
+      ...messages,
+      { role: "user", content: message },
+      { role: "assistant", content: "" },
+    ]);
+    setMessage("");
+
+    const response = fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify([...messages, { role: "user", content: message }]),
+    }).then(async (res) => {
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder();
+
+      let result = "";
+      return reader.read().then(function processText({ done, value }) {
+        if (done) {
+          return result;
+        }
+        const text = decoder.decode(value || new Uint8Array(), {
+          stream: true,
+        });
+
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1];
+          let otherMessages = messages.slice(0, messages.length - 1);
+
+          return [
+            ...otherMessages,
+            { ...lastMessage, content: lastMessage.content + text },
+          ];
+        });
+        return reader.read().then(processText);
+      });
+    });
+  };
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        direction="column"
+        width="500px"
+        height="700px"
+        border="1px solid black"
+        p={2}
+        spacing={3}
+      >
+        <Stack
+          direction="column"
+          spacing={2}
+          flexGrow={1}
+          overflow="auto"
+          maxHeight="100%"
+        >
+          {messages.map((message, index) => (
+            <Box
+              key={index}
+              display="flex"
+              justifyContent={
+                message.role === "assistant" ? "flex-start" : "flex-end"
+              }
+            >
+              <Box
+                bgcolor={
+                  message.role === "assistant"
+                    ? "primary.main"
+                    : "secondary.main"
+                }
+                color="white"
+                borderRadius={16}
+                p={3}
+              >
+                <ReactMarkdown>{message.content}</ReactMarkdown>
+              </Box>
+            </Box>
+          ))}
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <TextField
+            label="Message"
+            fullWidth
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          ></TextField>
+          <Button varianat="contained" onClick={sendMessage}>
+            {" "}
+            send
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
