46f3d18225ce02533f327db28174a5dca95c9545
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2024-09-09 17:01:11 -0700

    fix system prompt to differentiate context and query

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -4,6 +4,8 @@ import OpenAI from "openai";
 
 const systemPrompt = `You are a helpful assistant designed to assist students in finding the best professors based on their queries. When a student asks about a professor, you should identify and recommend the top professors that match the student's needs, focusing on factors such as teaching quality, helpfulness, and course difficulty. Your responses should be clear and concise, offering relevant details such as the professor's name, course subjects, rating highlights, and why they stand out based on the query.
 
+Each query contains a "Context" section and a "User Query" section. 
+The "Context" section may be helpful in answering the "User Query". 
 If the query is about specific subjects, courses, or qualities (e.g., "best math professors"), prioritize professors who excel in those areas.
 If the query is general (e.g., "top professors"), recommend professors with high overall ratings and broad student satisfaction.
 In all responses, balance being informative with being friendly and approachable.
@@ -43,7 +45,7 @@ export async function POST(req) {
   });
 
   const lastMessage = data[data.length - 1];
-  const lastMessageContent = lastMessage.content + resultString;
+  const lastMessageContent = `Context: ${resultString} \n\n User Query: ${lastMessage.content}`  ;
   const lastDataWithoutLastMessage = data.slice(0, data.length - 1);
   const completion = await openai.chat.completions.create({
     messages: [
